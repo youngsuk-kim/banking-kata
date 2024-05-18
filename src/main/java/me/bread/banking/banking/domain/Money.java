f5c0f0ae47dd228a4fffcbf955311ed8cb6c689a
stayforyou <bread.s@kakao.com>
2024-05-19 07:28:36 +0900

    implement money vo

--- src/main/java/me/bread/banking/banking/domain/Money.java
+++ src/main/java/me/bread/banking/banking/domain/Money.java
@@ -5,17 +5,17 @@ import java.util.Currency;
 
 public record Money(BigDecimal amount, Currency currency) {
 
-    public Money add(Money other) {
+    public Money add(final Money other) {
         checkCurrency(other);
         return new Money(amount.add(other.amount), currency);
     }
 
-    public Money subtract(Money other) {
+    public Money subtract(final Money other) {
         checkCurrency(other);
         return new Money(amount.subtract(other.amount), currency);
     }
 
-    private void checkCurrency(Money other) {
+    private void checkCurrency(final Money other) {
         if (!currency.equals(other.currency)) {
             throw new IllegalArgumentException("Cannot combine money of different currencies");
         }
