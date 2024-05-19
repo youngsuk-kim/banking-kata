424b21a31b6a4581d019f9ac35a13651b650ff1b
stayforyou <bread.s@kakao.com>
2024-05-19 14:27:53 +0900

    implement facade

--- src/main/java/me/bread/banking/banking/StartBanking.java
+++ src/main/java/me/bread/banking/banking/StartBanking.java
@@ -1,51 +1,17 @@
 package me.bread.banking.banking;
 
 import me.bread.banking.banking.domain.Account;
-import me.bread.banking.banking.domain.History;
 import me.bread.banking.banking.domain.Name;
-import me.bread.banking.banking.usecase.DepositUseCase;
-import me.bread.banking.banking.usecase.NewAccountUseCase;
-import me.bread.banking.banking.usecase.WithdrawUseCase;
-import me.bread.banking.banking.view.BodyView;
-import me.bread.banking.banking.view.HeaderView;
-import me.bread.banking.banking.view.model.BodyModel;
+import me.bread.banking.banking.facade.TransactionFacade;
 
 import java.math.BigDecimal;
-import java.util.Optional;
-
-import java.util.Random;
 
 public class StartBanking {
 
     public static void main(String[] args) {
         Name name = new Name("영석", "김");
-        Account account = new NewAccountUseCase().execute(name, new BigDecimal(10000));
-
-        printRandomAccountHistories(account, name);
-    }
-
-    private static void printRandomAccountHistories(Account account, Name name) {
-        Random random = new Random();
-        for (int i = 0; i < 100; i++) {
-            BigDecimal amount = BigDecimal.valueOf(random.nextInt(5000) + 100);
-            if (random.nextBoolean()) {
-                new DepositUseCase().execute(account, amount);
-            } else {
-                new WithdrawUseCase().execute(account, amount);
-            }
-        }
-
-        System.out.print(name.lastName());
-        System.out.println(name.firstName() + "의 입출금 내역");
-        HeaderView.print();
-        for (History history : account.getHistory()) {
-            BodyView.print(new BodyModel(
-                    Optional.of(history.date().toString()),
-                    Optional.ofNullable(history.credit()),
-                    Optional.ofNullable(history.debit()),
-                    Optional.ofNullable(history.balance())
-            ));
-        }
+        Account account = new Account(new BigDecimal(10000));
+        TransactionFacade.printRandomAccountHistories(account, name);
     }
 }
 
---
+++ src/main/java/me/bread/banking/banking/facade/TransactionFacade.java
@@ -0,0 +1,45 @@
+package me.bread.banking.banking.facade;
+
+import me.bread.banking.banking.domain.Account;
+import me.bread.banking.banking.domain.History;
+import me.bread.banking.banking.domain.Name;
+import me.bread.banking.banking.view.BodyView;
+import me.bread.banking.banking.view.HeaderView;
+import me.bread.banking.banking.view.model.BodyModel;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Random;
+
+public class TransactionFacade {
+    public static void printRandomAccountHistories(Account account, Name name) {
+        generateRandomTransactions(account);
+        printHistories(account, name);
+    }
+
+    private static void generateRandomTransactions(Account account) {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            BigDecimal amount = BigDecimal.valueOf(random.nextInt(5000) + 100);
+            if (random.nextBoolean()) {
+                account.deposit(amount);
+            } else {
+                account.withdraw(amount);
+            }
+        }
+    }
+
+    private static void printHistories(Account account, Name name) {
+        System.out.print(name.lastName());
+        System.out.println(name.firstName() + "의 입출금 내역");
+        HeaderView.print();
+        for (History history : account.getHistory()) {
+            BodyView.print(new BodyModel(
+                    Optional.of(history.date().toString()),
+                    Optional.ofNullable(history.credit()),
+                    Optional.ofNullable(history.debit()),
+                    Optional.ofNullable(history.balance())
+            ));
+        }
+    }
+}
